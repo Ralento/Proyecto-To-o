164389fc84d72e920924190521dbb635a4f52d64
Ralento <darckjessie@gmail.com>
2024-05-16 17:15:00 -0600

    ya supe que pedo

--- backend/index.php
+++ backend/index.php
@@ -1,14 +1,28 @@
 <?php
 
-$mysqli=new mysqli("localhost","root","","cp");
+$servidor = "localhost";
+$usuario = "root";
+$clave = "";
+$basededatos = "cp";
+$enlace = mysqli_connect($servidor, $usuario, $clave, $basededatos);
 
-$nombre=$_POST['nombre'];
-$apellido=$_POST['apellido'];
-$telefono=$_POST['telefono'];
-$correo=$_POST['correo'];
-$contra=$_POST['contrsaeña'];
-$query="INSERT INTO usuarios (nombre, apellido, contraseña, correo, telefono)
-        VALUES ('$nombre', '$apellido', '$telefono', '$correo', '$contra')";
-$resultado=$mysqli->query($query);
+if(isset($_POST['registro'])){
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $contraseña = $_POST['contraseña'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $insertarDatos = "INSERT INTO usuarios (nombre, apellido, contraseña, correo, telefono) VALUES ('$nombre', '$apellido', '$contraseña', '$correo', '$telefono')";
+    $ejecutarInsertar = mysqli_query($enlace, $insertarDatos);
+
+    if($ejecutarInsertar){
+        echo "Registro exitoso.";
+    } else {
+        echo "Error en el registro: " . mysqli_error($enlace);
+    }
+}
+
+header("Location: /frontend/login.html");
+exit();
 
 ?>

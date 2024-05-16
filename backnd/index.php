ac1530e865fe0707838f512434360750527bf7cd
Ralento <darckjessie@gmail.com>
2024-05-15 23:48:16 -0600

    intento de conexion jajj

--- backnd/index.php
+++ backnd/index.php
@@ -1,8 +1,17 @@
 <?php
 
-$conn = msqli_connect("localhost", "root", "", "ecp");
-if (!$conn)
+$serviodr = "localhost";
+$usuario = "root";
+$clave = "";
+$basededatos = "a";
+$enlace = mysqli_connect ($serviodr, $usuario, $clave, $basededatos);
 
-die("no jalo ");
+if(isset($_POST['registro'])){
+    $nombre =  $_POST ['nombre'];
+    $correo =  $_POST ['correo'];
+    $telefono =  $_POST ['telefono'];
+    $insertarDtos = "INSERT INTO datos VALUES('$nombre', '$correo', '$telefono', '')";
+    $ejecutarInsertar = mysqli_query ($enlace, $insertarDtos);
+}
 
 ?>

5cd821a9f68e3f6cf1277f4057e347933fa003b4
Ralento <darckjessie@gmail.com>
2024-05-19 01:55:56 -0600

    #

--- backend/index.php
+++ backend/index.php
@@ -90,7 +90,7 @@ $mysqli->close();
         <div class="row full-height justify-content-center">
           <div class="col-12 text-center align-self-center py-5">
             <div class="section pb-5 pt-5 pt-sm-2 text-center">
-              <h6 class="mb-0 pb-3"><span>Iniciar Sesion </span><span>Registrarse</span></h6>
+              <h6 class="mb-0 pb-3"><span>Iniciar Sesión </span><span>Registrarse</span></h6>
               <input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
               <label for="reg-log"></label>
               <div class="card-3d-wrap mx-auto">
@@ -98,7 +98,7 @@ $mysqli->close();
                   <div class="card-front">
                     <div class="center-wrap">
                       <div class="section text-center">
-                        <h4 class="mb-4 pb-3">Iniciar sesion</h4>
+                        <h4 class="mb-4 pb-3">Iniciar sesión</h4>
                         <div class="form-group">
                           <input type="text" class="form-style" placeholder="Usuario" name="login_usuario" value="<?php echo htmlspecialchars($login_usuario); ?>">
                           <i class="input-icon uil uil-at"></i>

e839a7db9b20d587a4cfc371d920da5a89dd68fd
Ralento <saulpool2305@gmail.com>
2024-06-06 22:43:21 -0600

    cambio del menu diseño de calculadoras inicio de la pestaña de perfil

--- frontend/imc/imc.js
+++ frontend/imc/imc.js
@@ -2,20 +2,11 @@ function calcularIMC() {
     const peso = document.getElementById('peso').value;
     const altura = document.getElementById('altura').value;
 
-    const alturaMetros = altura / 100; // Convertir la altura a metros
-    const imc = peso / (alturaMetros * alturaMetros);
-
-    let clasificacion;
-
-    if (imc < 18.5) {
-        clasificacion = 'Bajo peso';
-    } else if (imc >= 18.5 && imc < 24.9) {
-        clasificacion = 'Peso normal';
-    } else if (imc >= 25 && imc < 29.9) {
-        clasificacion = 'Sobrepeso';
+    if (peso && altura) {
+        const alturaMetros = altura / 100;
+        const imc = (peso / (alturaMetros * alturaMetros)).toFixed(2);
+        document.getElementById('imc').textContent = `Tu IMC es ${imc}`;
     } else {
-        clasificacion = 'Obesidad';
+        document.getElementById('imc').textContent = 'Por favor, introduce todos los datos';
     }
-
-    document.getElementById('imc').textContent = `Tu IMC es ${imc.toFixed(2)} (${clasificacion})`;
 }
